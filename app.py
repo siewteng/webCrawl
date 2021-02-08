42ee888377e0d8cbff68e57b2cff8cf928ac6671
siewteng <iamsiewteng@gmail.com>
2021-02-08 19:42:10 +0800

    comment body

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, render_template
 import praw
 import datetime
+from dateutil import tz
+
+# datetime stuff
+from_zone = tz.tzutc()
+to_zone = tz.tzlocal()
 
 reddit = praw.Reddit(client_id='bZnvlnGcW_cD_g',
                      client_secret='AmvzgAQojypX7OSILSDhfnIjGMIFlQ', user_agent='hootsnoow')
@@ -41,10 +46,10 @@ def index():
     # print("ID: " + submission.id)
 
     # still not sure how to put it in SG time
-    #dateTime = datetime.datetime.fromtimestamp(submission.created_utc)
-    #SGdateTime = dateTime.astimezone[datetime.tzname]
-    #print("timezone: " + str(datetime.datetime.tzname(self)))
-    #print("Time: " + str(datetime.datetime.fromtimestamp(submission.created_utc)))
+    # dateTime = datetime.datetime.fromtimestamp(submission.created_utc)
+    # SGdateTime = dateTime.astimezone[datetime.tzname]
+    # print("timezone: " + str(datetime.datetime.tzname(self)))
+    # print("Time: " + str(datetime.datetime.fromtimestamp(submission.created_utc)))
 
     # # this searches infinitely the comments of comments and so on until there are none left
     # submission.comments.replace_more(limit=0)
@@ -61,11 +66,28 @@ def my_form_post():
         url=theURL)
     submission.comments.replace_more(limit=0)
     mainPost = [submission.title, submission.selftext, str(submission.score)]
-    commentInfo = []
+    commentId = []
+    commentParent = []
+    commentAuthor = []
+    commentBody = []
+    commentTime = []
+    commentScore = []
     for comment in submission.comments.list():
-        commentInfo.append(comment)
+        commentId.append(comment)
+        commentParent.append(comment.parent_id)
+        commentAuthor.append(comment.author)
+        commentBody.append(comment.body)
+        commentScore.append(comment.score)
 
-    return render_template("index.html", mainPost=mainPost, commentInfo=commentInfo)
+        # configuring the datetime formatting
+        dateTime = datetime.datetime.fromtimestamp(comment.created_utc)
+        utc = dateTime.replace(tzinfo=from_zone)
+        SGdateTime = utc.replace(to_zone)
+        commentTime.append(SGdateTime)
+
+    # return render_template("index.html", mainPost=mainPost, commentId=commentId,
+    #                        commentParent=commentParent, commentAuthor=commentAuthor, commentBody=commentBody, commentScore=commentScore, commentTime=commentTime)
+    return render_template("index.html", mainPost=mainPost, commentBody=commentBody)
 
 
 if __name__ == "__main__":
